d04f9ed8f32af021a1c293702dddcef3ff4aadc1
mnesicles <benjamindelgado.contacto@gmail.com>
2024-05-05 03:37:13 -0300

    crea las provincias al iniciar el programa

--- src/main.pas
+++ src/main.pas
@@ -2,8 +2,11 @@
 
 program main;
 
-uses menu_principal in 'src/views/menu_principal.pas';
+uses 
+    menu_principal in 'src/views/menu_principal.pas',
+    provincia_controller in 'src/controllers/provincia_controller.pas';
 
 begin
+    crearLasProvincias();
     PintarMenuPrincipal();
 end.
