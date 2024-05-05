370054a68ac3397052d7a3fe0dc403eafc93c6b0
mnesicles <benjamindelgado.contacto@gmail.com>
2024-05-05 03:23:21 -0300

    Crea modelo de provincia

---
+++ src/models/provincia_model.pas
@@ -0,0 +1,15 @@
+unit provincia_model;
+
+interface
+
+type TProvincia = record
+    cod_provincia: string;
+    denominacion: string;
+    telefono_turismo: string;
+end;
+
+type TListaDeProvincias = array of TProvincia;
+
+implementation
+    
+end.
